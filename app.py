1d937d9e52b44cbe4a5d11047b9ac6e16385161f
morris1231 <72624849+morris1231@users.noreply.github.com>
2025-09-17 14:44:20 +0200

    Update app.py

--- app.py
+++ app.py
@@ -69,4 +69,5 @@ def webex_webhook():
     return {"status": "ignored"}, 200
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=5000, debug=True)
+
+    app.run(host="0.0.0.0", port=5000, debug=True)
